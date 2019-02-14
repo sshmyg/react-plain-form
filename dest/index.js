63556b45a8db19ac5737419e97ed8fb911d74e62
Serhey Shmyg <serhey.shmyg@gmail.com>
2019-02-14 11:51:38 +0200

    Hooks

---
+++ dest/index.js
@@ -0,0 +1,218 @@
+module.exports =
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, { enumerable: true, get: getter });
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// define __esModule on exports
+/******/ 	__webpack_require__.r = function(exports) {
+/******/ 		if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 			Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 		}
+/******/ 		Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 	};
+/******/
+/******/ 	// create a fake namespace object
+/******/ 	// mode & 1: value is a module id, require it
+/******/ 	// mode & 2: merge all properties of value into the ns
+/******/ 	// mode & 4: return value when already ns object
+/******/ 	// mode & 8|1: behave like require
+/******/ 	__webpack_require__.t = function(value, mode) {
+/******/ 		if(mode & 1) value = __webpack_require__(value);
+/******/ 		if(mode & 8) return value;
+/******/ 		if((mode & 4) && typeof value === 'object' && value && value.__esModule) return value;
+/******/ 		var ns = Object.create(null);
+/******/ 		__webpack_require__.r(ns);
+/******/ 		Object.defineProperty(ns, 'default', { enumerable: true, value: value });
+/******/ 		if(mode & 2 && typeof value != 'string') for(var key in value) __webpack_require__.d(ns, key, function(key) { return value[key]; }.bind(null, key));
+/******/ 		return ns;
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = "./src/index.js");
+/******/ })
+/************************************************************************/
+/******/ ({
+
+/***/ "./src/index.js":
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+/*! exports provided: useForm */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "useForm", function() { return useForm; });
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "react");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+function _objectSpread(target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i] != null ? arguments[i] : {}; var ownKeys = Object.keys(source); if (typeof Object.getOwnPropertySymbols === 'function') { ownKeys = ownKeys.concat(Object.getOwnPropertySymbols(source).filter(function (sym) { return Object.getOwnPropertyDescriptor(source, sym).enumerable; })); } ownKeys.forEach(function (key) { _defineProperty(target, key, source[key]); }); } return target; }
+
+function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
+
+function _slicedToArray(arr, i) { return _arrayWithHoles(arr) || _iterableToArrayLimit(arr, i) || _nonIterableRest(); }
+
+function _nonIterableRest() { throw new TypeError("Invalid attempt to destructure non-iterable instance"); }
+
+function _iterableToArrayLimit(arr, i) { var _arr = []; var _n = true; var _d = false; var _e = undefined; try { for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i["return"] != null) _i["return"](); } finally { if (_d) throw _e; } } return _arr; }
+
+function _arrayWithHoles(arr) { if (Array.isArray(arr)) return arr; }
+
+function _objectWithoutProperties(source, excluded) { if (source == null) return {}; var target = _objectWithoutPropertiesLoose(source, excluded); var key, i; if (Object.getOwnPropertySymbols) { var sourceSymbolKeys = Object.getOwnPropertySymbols(source); for (i = 0; i < sourceSymbolKeys.length; i++) { key = sourceSymbolKeys[i]; if (excluded.indexOf(key) >= 0) continue; if (!Object.prototype.propertyIsEnumerable.call(source, key)) continue; target[key] = source[key]; } } return target; }
+
+function _objectWithoutPropertiesLoose(source, excluded) { if (source == null) return {}; var target = {}; var sourceKeys = Object.keys(source); var key, i; for (i = 0; i < sourceKeys.length; i++) { key = sourceKeys[i]; if (excluded.indexOf(key) >= 0) continue; target[key] = source[key]; } return target; }
+
+
+/**
+ * useField
+ * @param {Object} props
+ * @param {String|Number} [props.defaultValue = '']
+ * @param {Function} [props.handleValidation = () => true]
+ * @returns [
+ *      {} //Valid DOM attrs,
+ *      {} //Component props
+ * ]
+ */
+
+function useField(props) {
+  var _props$type = props.type,
+      type = _props$type === void 0 ? 'text' : _props$type,
+      _props$defaultValue = props.defaultValue,
+      defaultValue = _props$defaultValue === void 0 ? '' : _props$defaultValue,
+      _props$handleValidati = props.handleValidation,
+      handleValidation = _props$handleValidati === void 0 ? function () {
+    return true;
+  } : _props$handleValidati,
+      rest = _objectWithoutProperties(props, ["type", "defaultValue", "handleValidation"]);
+
+  var _useState = Object(react__WEBPACK_IMPORTED_MODULE_0__["useState"])(),
+      _useState2 = _slicedToArray(_useState, 2),
+      error = _useState2[0],
+      setError = _useState2[1];
+
+  var _useState3 = Object(react__WEBPACK_IMPORTED_MODULE_0__["useState"])(String(defaultValue)),
+      _useState4 = _slicedToArray(_useState3, 2),
+      value = _useState4[0],
+      setValue = _useState4[1];
+
+  return [//Valid DOM attrs
+  _objectSpread({
+    type: type,
+    value: value
+  }, rest, {
+    onChange: function onChange(e) {
+      setValue(e.target.value);
+    },
+    onBlur: function onBlur(e) {},
+    onFocus: function onFocus(e) {}
+  }), //Component props
+  {
+    error: error,
+    setError: setError,
+    setValue: setValue,
+    handleValidation: handleValidation
+  }];
+}
+/**
+ * useForm
+ * @param {Array} fields
+ */
+
+
+function useForm(fields) {
+  var res = Object.keys(fields).reduce(function (acc, name) {
+    var field = fields[name];
+
+    var _useField = useField(field),
+        _useField2 = _slicedToArray(_useField, 2),
+        attrs = _useField2[0],
+        props = _useField2[1];
+
+    acc.fields[name] = attrs;
+    acc.values[name] = attrs.value;
+    acc.validations.push(props.handleValidation);
+    return acc;
+  }, {
+    fields: {},
+    values: {},
+    validations: []
+  });
+  return _objectSpread({}, res, {
+    handleSubmit: function handleSubmit() {
+      var values = res.values,
+          validations = res.validations;
+      validations.forEach(function (validate) {
+        return validate(values);
+      });
+      return values;
+    }
+  });
+}
+
+/***/ }),
+
+/***/ "react":
+/*!************************!*\
+  !*** external "react" ***!
+  \************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+module.exports = require("react");
+
+/***/ })
+
+/******/ });
--- src/index.js
+++ src/index.js
@@ -1,5 +1,85 @@
-import Form from './Form';
-import withForm from './withForm';
+import {
+    useState
+} from 'react';
 
-export default withForm(Form);
-export * from './withForm';
+/**
+ * useField
+ * @param {Object} props
+ * @param {String|Number} [props.defaultValue = '']
+ * @param {Function} [props.handleValidation = () => true]
+ * @returns [
+ *      {} //Valid DOM attrs,
+ *      {} //Component props
+ * ]
+ */
+function useField(props) {
+    const {
+        type = 'text',
+        defaultValue = '',
+        handleValidation = () => true,
+        ...rest
+    } = props;
+    const [error, setError] = useState();
+    const [value, setValue] = useState(String(defaultValue));
+
+    return [
+        //Valid DOM attrs
+        {
+            type,
+            value,
+            ...rest,
+            onChange: e => {
+                setValue(e.target.value);
+            },
+            onBlur: e => {
+
+            },
+            onFocus: e => {
+
+            }
+        },
+
+        //Component props
+        {
+            error,
+            setError,
+            setValue,
+            handleValidation
+        }
+    ];
+}
+
+/**
+ * useForm
+ * @param {Array} fields
+ */
+export function useForm(fields) {
+    const res = Object.keys(fields).reduce((acc, name) => {
+        const field = fields[name];
+
+        const [attrs, props] = useField(field);
+        acc.fields[name] = attrs;
+        acc.values[name] = attrs.value;
+        acc.validations.push(props.handleValidation);
+
+        return acc;
+    }, {
+        fields: {},
+        values: {},
+        validations: []
+    });
+
+    return {
+        ...res,
+        handleSubmit: () => {
+            const {
+                values,
+                validations
+            } = res;
+
+            validations.forEach(validate => validate(values));
+
+            return values;
+        }
+    };
+}
